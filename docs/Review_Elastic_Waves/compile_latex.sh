76aff6a2c551b94b54b7d101521397ac0c4e03f1
John Doe <oandres.rincon@udea.edu.co>
2024-07-31 16:35:41 -0500

    chore: Update fonts and figures directory structure

--- docs/Review_Elastic_Waves/compile_latex.sh
+++ docs/Review_Elastic_Waves/compile_latex.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Compilar el archivo .tex con xelatex
-xelatex Review_Elastic_Waves.tex
+xelatex main_review_seismic_waves.tex
 
 # Ejecutar bibtex para procesar las referencias
-bibtex Review_Elastic_Waves.aux
+bibtex main_review_seismic_waves.aux
 
 # Compilar dos veces más con xelatex para asegurar que todas las referencias se resuelvan correctamente
-xelatex Review_Elastic_Waves.tex
-xelatex Review_Elastic_Waves.tex
+xelatex main_review_seismic_waves.tex
+xelatex main_review_seismic_waves.tex
