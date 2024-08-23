ff5fb46d9847693bb06397eff2a49f60d9f3d082
John Doe <oandres.rincon@udea.edu.co>
2024-08-23 18:02:56 -0500

    Refactor LaTeX configuration for matplotlib

--- utils/plotting.py
+++ utils/plotting.py
@@ -29,20 +29,21 @@ def figsize(width_scale=1, height_scale=1, nplots=1):
     fig_size = [fig_width, fig_height]
     return fig_size
 
+# Configuración de LaTeX para matplotlib
 pgf_with_latex = {                      # setup matplotlib to use latex for output
-    "pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
+    "pgf.texsystem": "xelatex",        # change this if using xetex or lautex
     "text.usetex": True,                # use LaTeX to write all text
-    "font.family": "serif",
-    "font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
-    "font.sans-serif": [],
+    "font.family": "sans-serif",
+    "font.serif": [],
+    "font.sans-serif": ["DejaVu Sans"], # specify the sans-serif font
     "font.monospace": [],
-    "axes.labelsize": 10,               # LaTeX default is 10pt font.
+    "axes.labelsize": 8,               # LaTeX default is 10pt font.
     "font.size": 10,
     "legend.fontsize": 8,               # Make the legend/label fonts a little smaller
     "xtick.labelsize": 8,
     "ytick.labelsize": 8,
-    "figure.figsize": figsize(1.0),     # default fig size of 0.9 textwidth
-    "pgf.preamble": r'\usepackage[utf8x]{inputenc},\usepackage[T1]{fontenc},\usepackage{amssymb},\usepackage{amsfonts}',
+    "figure.figsize": (3.15, 2.17),     # default fig size of 0.9 textwidth
+    "pgf.preamble": r' \usepackage{amsmath},\usepackage{cmbright},\usepackage[utf8x]{inputenc},\usepackage[T1]{fontenc},\usepackage{amssymb},\usepackage{amsfonts},\usepackage{mathastext}',
         # plots will be generated using this preamble
     }
 mpl.rcParams.update(pgf_with_latex)
